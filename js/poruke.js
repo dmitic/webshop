57fa17ab1202735934805dfc90f15177bc8f1d18
Dragan Mitic <dmitic@gmail.com>
2019-08-02 15:05:14 +0200

    Small changes

--- js/poruke.js
+++ js/poruke.js
@@ -3,9 +3,7 @@ const poruke = {
   const div = document.createElement('div');
   div.className = `msg ${boja}`;
   div.append(document.createTextNode(poruka));
-  const forma_rod = document.querySelector('.container');
-  const el_ispod = document.querySelector('.proizvodi');
-  forma_rod.insertBefore(div, el_ispod);
+  document.body.append(div);
 
   setTimeout(function(){
     document.querySelector('.msg').remove();
