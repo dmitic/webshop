eb303c2af93a7402de9de5045c1c535ec3e0adcd
Dragan Mitic <dmitic@gmail.com>
2019-08-02 14:58:43 +0200

    Typos

--- js/poruke.js
+++ js/poruke.js
@@ -1,10 +1,10 @@
 const poruke = {
   poruka: function(poruka, boja) {
-  let div = document.createElement('div');
+  const div = document.createElement('div');
   div.className = `msg ${boja}`;
   div.append(document.createTextNode(poruka));
-  let forma_rod = document.querySelector('.container');
-  let el_ispod = document.querySelector('.proizvodi');
+  const forma_rod = document.querySelector('.container');
+  const el_ispod = document.querySelector('.proizvodi');
   forma_rod.insertBefore(div, el_ispod);
 
   setTimeout(function(){
